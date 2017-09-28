44429f575bcac2840d7502c812ef9d0fb6fb8856
Norwin Guerreo <norwingcruz@gmail.com>
2017-09-28 16:49:13 -0600

    done

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -5,17 +5,48 @@
                 <router-link to="/">Home</router-link>
             </div>
             <ul class="navbar__list">
-                <li class="navbar__item">
+                <li class="navbar__item" v-if="!auth.api_token">
                     <router-link to="/login">Login</router-link>
                 </li>
-                <li class="navbar__item">
+                <li class="navbar__item" v-if="!auth.api_token">
                     <router-link to="/register">Register</router-link>
                 </li>
-                <li class="navbar__item">
-                    <router-link to="/logout">Logout</router-link>
+                <li class="navbar__item" v-if="auth.api_token">
+                    <a @click.stop="logout">Logout</a>
                 </li>
             </ul>
         </div>
+        <div class="flash flash__success" v-if="flash.success">{{ flash.success }}</div>
+        <div class="flash flash__error" v-if="flash.error">{{ flash.error }}</div>
         <router-view></router-view>
     </div>
 </template>
+
+<script type="text/javascript">
+    import Flash from './helpers/flash'
+    import Auth from './store/auth'
+    import { post } from './helpers/api'
+
+    export default{
+        created(){
+            Auth.initialize()
+        },
+        data(){
+            return{
+                flash: Flash.state,
+                auth : Auth.state
+            }
+        },
+        methods: {
+            logout(){
+                post('api/logout').then((res) => {
+                    if(res.data.done){
+                        Auth.remove()
+                        Flash.setSuccess('You have successfully logged out!')
+                        this.$router.push('/login')
+                    }
+                })
+            }
+        }
+    }
+</script>
--- resources/assets/js/router/index.js
+++ resources/assets/js/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Register from '../views/auth/register.vue'
+import Login from '../views/auth/login.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
     routes:[
-
+        { path: '/register', component: Register },
+        { path: '/login', component: Login }
     ]
 })
 
